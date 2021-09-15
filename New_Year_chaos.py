bdecb9e3f0b3cc4f17aec5e1dea7102679bf4e0d
Eduardo Sanchez <esanchez.h25@gmail.com>
2021-09-15 12:08:58 -0500

    backup

--- New_Year_chaos.py
+++ New_Year_chaos.py
@@ -1,19 +1,13 @@
-q = [1,2,5,3,7,8,6,4]
-dic = {}
+q = [1,2,5,3,4,7,8,6]
 res = 0
 n = len(q)
 for i in range(n):
-    for j in range(n-1):
-        if q[j] > q[j + 1]:
-            if q[j] in dic:
-                dic[q[j]] += 1
-                if dic[q[j]] > 2:
-                    print('Too chaotic')
-                    break
-            else:
-                dic[q[j]] = 1
-            aux = q[j]
-            q[j] = q[j + 1]
-            q[j + 1] = aux
+    aux = q[i] - (i + 1)
+    if aux > 2:
+        print('Too chaotic')
+        break
+    for j in range(max(0,q[i] - 2), i):
+        if q[j] > q[i]:
             res += 1
+
 print(res)
